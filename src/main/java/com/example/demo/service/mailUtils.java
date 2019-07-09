3200c6d4f91a87f2b67e2e961554edce70ea65f9
Yangzh888 <1121095915@qq.com>
2019-07-09 17:05:42 +0800

    新增多个发送不同邮件方法

--- src/main/java/com/example/demo/service/mailUtils.java
+++ src/main/java/com/example/demo/service/mailUtils.java
@@ -1,17 +1,36 @@
 package com.example.demo.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+/**
+ * 这是一个发送邮件的工具类
+ * TODO 暂未封装为接口，具体使用请到sendMailTest.java文件测试使用
+ *
+ */
 @Service
 public class mailUtils {
+
+
+    /**
+     * 以发送HTML邮件为例使用logger打印日志
+     */
+    private final Logger logger= LoggerFactory.getLogger(this.getClass());
 
     @Value("${spring.mail.username}")
     private String form;
@@ -36,18 +55,68 @@ public class mailUtils {
 
     /**
      * 发送HTML邮件
-     * @param to
-     * @param subject
-     * @param content
+     * @param to 要发送给的邮箱地址
+     * @param subject 发送的主题
+     * @param content 发送的内容
      * @throws MessagingException
      */
-    public void sendHtmlMail(String to,String subject,String content ) throws MessagingException {
+    public void sendHtmlMail(String to,String subject,String content ) {
+        logger.info("发送HTML邮件开始：{}，{},{}",to,subject,content);
+        MimeMessage message=javaMailSender.createMimeMessage();
+        MimeMessageHelper messageHelper= null;
+        try {
+            messageHelper = new MimeMessageHelper(message,true);
+            messageHelper.setFrom(form);
+            messageHelper.setTo(to);
+            messageHelper.setSubject(subject);
+            messageHelper.setText(content,true);//是否为HTML
+            javaMailSender.send(message);
+            logger.info("发送HTML邮件发送成功");
+        } catch (MessagingException e) {
+            logger.info("发送HTML邮件发送失败",e);
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 发送一封带有附件的HTML邮件
+     * @param to 要发送给的邮箱地址
+     * @param subject 发送的主题
+     * @param content 发送的内容
+     * @param filePath 文件的路径.可以填写一个或者多个路径
+     * @throws MessagingException
+     */
+    public void sendAttachmentsMail(String to,String subject, String content,
+                                     String... filePath) throws MessagingException {
         MimeMessage message=javaMailSender.createMimeMessage();
         MimeMessageHelper messageHelper=new MimeMessageHelper(message,true);
         messageHelper.setFrom(form);
         messageHelper.setTo(to);
         messageHelper.setSubject(subject);
         messageHelper.setText(content,true);//是否为HTML
+        if(filePath!=null){
+            List<String> resultList = new ArrayList<>(filePath.length);
+            Collections.addAll(resultList,filePath);
+            for (String s:resultList){
+                FileSystemResource file=new FileSystemResource(new File(s));//通过文件路径创建一个流的临时地址
+                String fileName=file.getFilename(); //获取文件的名字
+                messageHelper.addAttachment(fileName,file); //将附件的信息添加进去
+            }
+        }
+        javaMailSender.send(message);
+    }
+
+    public void sendInlinkResourceMail(String to,String subject, String content,
+                                       String rscPath,String rscId) throws MessagingException {
+        MimeMessage message=javaMailSender.createMimeMessage();
+        MimeMessageHelper messageHelper=new MimeMessageHelper(message,true);
+        messageHelper.setFrom(form);
+        messageHelper.setTo(to);
+        messageHelper.setSubject(subject);
+        messageHelper.setText(content,true);//是否为HTML
+        FileSystemResource res=new FileSystemResource(new File(rscPath));
+        messageHelper.addInline(rscId,res);
         javaMailSender.send(message);
     }
 }
--- src/test/java/com/example/demo/sendMailTest.java
+++ src/test/java/com/example/demo/sendMailTest.java
@@ -5,9 +5,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.annotation.Resource;
 import javax.mail.MessagingException;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,12 +18,14 @@ public class sendMailTest {
     @Resource
     private mailUtils utils;
 
+    @Resource
+    TemplateEngine templateEngine;      //模板引擎，来自于thymeleaf
     /**
      * 测试发送简单邮件
      */
     @Test
     public void sendSimpleMailTset(){
-        String to="**@qq.com";//修改为要发送的邮箱地址
+        String to="";//修改为要发送的邮箱地址
         String subject="第一封简单邮件";//修改为要发送的邮件主题
         String content="测试主题";//修改为要发送的邮件内容
         utils.sendSimpleMail(to,subject,content);
@@ -32,7 +37,7 @@ public class sendMailTest {
      */
     @Test
     public void sendHtmlMailTest() throws MessagingException {
-        String to="**@qq.com";//修改为要发送的邮箱地址
+        String to="";//修改为要发送的邮箱地址
         String subject="测试发送一封HTML的邮件";//修改为要发送的邮件信息
         String content="<html>\n"+
                 "<body>\n"+
@@ -41,4 +46,50 @@ public class sendMailTest {
                 "</html>";     //设置邮件内容为HTML格式
         utils.sendHtmlMail(to,subject,content);
     }
+
+    /**
+     * 测试发送一封带有附件的HTML邮件
+     * 若不带path参数即不发送附件
+     * @throws MessagingException
+     */
+    @Test
+    public void sendAttachementHtmlMailTest() throws MessagingException {
+        String path="com.example.demo.sendMailTest";//相对路径或者绝对路径
+        String to="";//修改为要发送的邮箱地址
+        String subject="测试发送一封HTML的邮件";//修改为要发送的邮件信息
+        String content="<html>\n"+
+                "<body>\n"+
+                "<h3>hello world,这是一封HTML邮件！"+"</h3>"+
+                "</body>\n"+
+                "</html>";     //设置邮件内容为HTML格式
+        utils.sendAttachmentsMail(to,subject,content,path);
+    }
+    @Test
+    public void sendInlinkResourceMailTest() throws MessagingException {
+        String imgPath="D:/test.jpg";  //图片的地址如"D:/gitProject/MailUtils/src/test/java/com/example/demo/sendMailTest.java"绝对路径
+        String to="1121095915@qq.com";//修改为要发送的邮箱地址
+        String subject="测试发送一封带有图片的HTML的邮件";//修改为要发送的邮件信息
+        String rscId="neo001";//给图片定义一个ID
+        /**
+         * //修改html样式此处修改
+         * //需要发送多个相同图片修改HTML方式即可。
+         * //多个不同图片修改发送方法为类似附件那种即可
+         */
+        String content="<HTML><body>这是有图片的邮件：<img src=\'cid:"+rscId
+                         +"\'></img></body></html>";
+
+        utils.sendInlinkResourceMail(to,subject,content,imgPath,rscId);
+  }
+
+  public void testTemplateMailTest() throws MessagingException {
+      String to="1121095915@qq.com";//修改为要发送的邮箱地址
+      String subject="测试发送一封HTML的邮件";//修改为要发送的邮件信息
+        Context context=new Context();
+        //页面需要传输的参数
+        context.setVariable("id","006");
+        //process第一个参数为页面的名字，第二个则为配置好的参数
+        String emailContent=templateEngine.process("emailTemplate",context);
+        //通过发送普通html邮件的方法把html页面传入
+        utils.sendHtmlMail(to,subject,emailContent);
+  }
 }
